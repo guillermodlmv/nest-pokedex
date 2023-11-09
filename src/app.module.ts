b9119add3128ad971f4a9fb71722721a768a9568
guillermodlmv <guillermodevillagomez@gmail.com>
2023-11-08 19:43:42 -0600

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
